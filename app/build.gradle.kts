1bdde0cce4541ba878ac78d973a607f56601b01c
wjddn <5000batt@gmail.com>
2025-10-14 16:21:11 +0900

    chore: minSdkVersion 상향에 따른 라이브러리 버전 일괄 조정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kjw.fridgerecipe"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
