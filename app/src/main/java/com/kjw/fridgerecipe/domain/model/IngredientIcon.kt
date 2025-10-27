17ddf2d61976ca8aa7ed764d0e180938ee4babd4
wjddn <5000batt@gmail.com>
2025-10-27 14:08:30 +0900

    feat: 임시 기본 이미지 파일 추가 및 Icon enum class 수정

--- app/src/main/java/com/kjw/fridgerecipe/domain/model/IngredientIcon.kt
+++ app/src/main/java/com/kjw/fridgerecipe/domain/model/IngredientIcon.kt
@@ -5,5 +5,5 @@ import com.kjw.fridgerecipe.R
 
 enum class IngredientIcon(@DrawableRes val iconResId: Int, val description: String) {
     CARROT(R.drawable.ic_launcher_foreground, "당근"),
-    DEFAULT(R.drawable.ic_launcher_foreground, "기본 아이콘")
+    DEFAULT(R.drawable.default_image, "기본")
 }
