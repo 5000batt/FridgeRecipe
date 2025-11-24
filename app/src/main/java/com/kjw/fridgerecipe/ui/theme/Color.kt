e6440f5da4ba501ab5bcf37786b065775bc4ad56
wjddn <5000batt@gmail.com>
2025-11-24 18:40:40 +0900

    style: Green 테마 적용

--- app/src/main/java/com/kjw/fridgerecipe/ui/theme/Color.kt
+++ app/src/main/java/com/kjw/fridgerecipe/ui/theme/Color.kt
@@ -2,10 +2,36 @@ package com.kjw.fridgerecipe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+var GreenPrimary = Color(0xFF43A047)
+val GreenOnPrimary = Color(0xFFFFFFFF)
+val GreenPrimaryContainer = Color(0xFFC8E6C9)
+val GreenOnPrimaryContainer = Color(0xFF1B5E20)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LimeSecondary = Color(0xFF7CB342)
+val LimeOnSecondary = Color(0xFFFFFFFF)
+val LimeSecondaryContainer = Color(0xFFDCEDC8)
+val LimeOnSecondaryContainer = Color(0xFF33691E)
+
+val BackgroundLight = Color(0xFFFAFAFA)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnSurfaceLight = Color(0xFF191C1B)
+
+// Dark Mode
+val GreenPrimaryDark = Color(0xFF81C784)
+val GreenOnPrimaryDark = Color(0xFF1B5E20)
+val GreenPrimaryContainerDark = Color(0xFF2E7D32)
+
+val LimeSecondaryDark = Color(0xFFAED581)
+val LimeOnSecondaryDark = Color(0xFF33691E)
+val LimeSecondaryContainerDark = Color(0xFF558B2F)
+
+val BackgroundDark = Color(0xFF191C1B)
+val SurfaceDark = Color(0xFF202322)
+val OnSurfaceDark = Color(0xFFE1E3DF)
+
+// Surface Variant
+val GreenSurfaceVariantLight = Color(0xFFDEE5D9)
+val GreenOnSurfaceVariantLight = Color(0xFF424940)
+
+// Outline
+val GreenOutlineLight = Color(0xFF72796F)
