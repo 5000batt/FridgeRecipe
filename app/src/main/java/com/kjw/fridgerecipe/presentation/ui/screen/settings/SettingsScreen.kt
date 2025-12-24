7ddfc17d9553e304e78e2571fea790bbfb26ae8f
wjddn <5000batt@gmail.com>
2025-12-25 03:48:34 +0900

    refactor: 환경설정 화면 하단 여백 수정

--- app/src/main/java/com/kjw/fridgerecipe/presentation/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/kjw/fridgerecipe/presentation/ui/screen/settings/SettingsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -97,6 +98,7 @@ fun SettingsScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .navigationBarsPadding()
                 .verticalScroll(rememberScrollState())
         ) {
             SettingsSectionTitle(stringResource(R.string.settings_section_notification))
