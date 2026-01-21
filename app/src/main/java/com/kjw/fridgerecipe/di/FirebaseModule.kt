2f2ae590a734cc4114cdac646a50298d16e3e623
wjddn <5000batt@gmail.com>
2026-01-21 16:36:17 +0900

    refactor: Remote Config 내 하드코딩된 광고 ID 제거

--- app/src/main/java/com/kjw/fridgerecipe/di/FirebaseModule.kt
+++ app/src/main/java/com/kjw/fridgerecipe/di/FirebaseModule.kt
@@ -25,13 +25,6 @@ object FirebaseModule {
         }
         remoteConfig.setConfigSettingsAsync(configSettings)
 
-        val defaultDefaults = mapOf(
-            "gemini_model_endpoint" to "v1beta/models/gemini-2.5-flash-lite:generateContent",
-            "admob_banner_id" to "ca-app-pub-2909354341085431/6849543362",
-            "admob_reward_id" to "ca-app-pub-2909354341085431/6649221055"
-        )
-        remoteConfig.setDefaultsAsync(defaultDefaults)
-
         return remoteConfig
     }
 }
